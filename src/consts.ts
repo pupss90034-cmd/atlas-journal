524de9f772e006ec290d75a24b6aa6871493538f
Niki <pupss90034@gmail.com>
2026-07-27 19:04:34 +0200

    加上 noindex、新增 Obsidian 文章範本、修正圖片路徑、版面改為左右並列的旅行風格排版

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,7 @@
 
 export const SITE_TITLE = "Atlas Journal";
 export const SITE_DESCRIPTION = "紀錄露營車生活、旅行與工作路上的大小事。";
+
+// 網站尚未準備好給搜尋引擎收錄時設為 false（會在所有頁面加上 noindex）。
+// 準備好要公開被 Google 搜尋到時，把這個改成 true 即可。
+export const SITE_IS_PUBLISHED = false;
